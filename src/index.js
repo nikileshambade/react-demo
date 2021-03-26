e248e20f81421d979ed3aded513607fcb822c7bb
NikhileshAmbade <Nikhilesh.Ambade@kibocommerce.com>
2021-03-26 10:19:31 +0530

    initial setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>Hello React Demo</div>,
+  document.getElementById('root')
+)
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"]
+        }
+      },
+      {
+        test: /\.s[ac]ss?$/,
+        exclude: /node_modules/,
+        use: [
+          "style-loader",
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'index.html') })]
+};
